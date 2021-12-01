57a8097babdf76b72558db1e9dbccaf224c53c89
Jennifer <jenniferh7637@gmail.com>
2021-12-01 01:01:32 -0300

    implementa o getAllVaccines, o getVaccine e o updateVaccine

--- src/routes/vaccines.js
+++ src/routes/vaccines.js
@@ -3,5 +3,9 @@ const router = express.Router()
 const controller = require("../controllers/vaccineController")
 
 router.post("/", controller.createVaccine)
+router.get("/", controller.getAllVaccines)
+router.get("/:id", controller.getVaccine)
+router.patch("/:id", controller.updateVaccinated)
 
-module.exports = router;
+
+module.exports = router
